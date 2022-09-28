e8b8dbf15da187c0c13c26a92c1038ed2caf278e
matt-grimsley <rmattgrimsley@gmail.com>
2022-09-27 23:25:49 -0500

    chapter 6 and 7

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs';
+import { IProduct } from './product';
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = 'cart';
-  products: any[] = [
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
+  }
+
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [
     {
       productId: 2,
       productName: 'Garden Cart',
@@ -33,7 +45,18 @@ export class ProductListComponent {
     },
   ];
 
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy)
+    );
+  }
+
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void {
+    this.listFilter = 'cart';
   }
 }
